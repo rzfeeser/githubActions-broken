3faa048b3da48f914deccedc448ed40220079dff
RZFeeser <rzfeeser@users.noreply.github.com>
2025-08-13 08:44:28 -0400

    Create db.py

---
+++ db.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def main():
+   """code to connect to db backend"""
+   print("Connecting to db")
+   print("connected!")
+   print("doin stuff to the backend yada yada")
+
+if __name__ == "__main__":
+   main()
